a5e39b869fa8983ddac7ae52d93b2889ebd36896
Jayshri Patil <jayshripatil743@gmail.com>
2023-05-02 11:43:22 +0530

    44 graph problem

---
+++ Daily Problem/Graph/Number of Provinces.cpp
@@ -0,0 +1,51 @@
+// link:- https://practice.geeksforgeeks.org/problems/number-of-provinces/1
+// tc:- 
+
+// approach 1
+
+class Solution {
+  public:
+  
+  void dfs(int node, vector<int> adjls[], int vis[])
+  {
+      vis[node] = 1;
+      
+      for(auto it : adjls[node])
+      {
+          if(!vis[it])
+          {
+              dfs(it, adjls, vis);
+          }
+      }
+  }
+  
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+        vector<int> adjls[V];
+        
+        for(int i = 0; i < V; i++)
+        {
+            for(int j = 0; j < V; j++)
+            {
+                if(adj[i][j] == 1 && i != j)
+                {
+                    adjls[i].push_back(j);
+                    adjls[j].push_back(i);
+                }
+            }
+        }
+        
+        int vis[V] = {0};
+        int count = 0;
+        
+        for(int i = 0; i < V; i++)
+        {
+            if(!vis[i])
+            {
+                count++;
+                dfs(i, adjls, vis);
+            }
+        }
+        return count;
+    }
+};
