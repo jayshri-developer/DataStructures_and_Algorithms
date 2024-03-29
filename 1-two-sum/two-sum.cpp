eb34cd113a76e0e6628aa0ff4c8fa7821958962f
jayshri-developer <55145381+jayshri-developer@users.noreply.github.com>
2024-03-29 17:24:51 +0530

    Time: 236 ms (6.17%) | Memory: 12.7 MB (74.00%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,19 +3,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
 
-        for(int i = 0; i < nums.size(); i++)
+        for(int i = 0 ; i < nums.size(); i++)
         {
             for(int j = 0; j < nums.size(); j++)
             {
-                if(i != j)
+                if( i != j)
                 {
-                    int sum = nums[i] + nums[j];
-                    if(sum == target)
+                    if(nums[i] + nums[j] == target)
                     {
                         ans.push_back(i);
                         ans.push_back(j);
-                        // cout<<"hello break plz"<<i<<"->"<<j<<":::";
-                        // break;
                         return ans;
                     }
                 }
