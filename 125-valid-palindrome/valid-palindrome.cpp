dc97b7969feec12f0d665f22b27ba92d3edb2308
jayshri-developer <55145381+jayshri-developer@users.noreply.github.com>
2024-06-27 20:16:27 +0530

    Time: 6 ms (45.99%) | Memory: 9.9 MB (6.30%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        stack<char> st;
+        string  ans = "";
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9') || (s[i] >= 'A' && s[i] <= 'Z'))
+            {
+                ans.push_back(tolower(s[i])); 
+            }
+            // else
+            // {
+            //     continue;
+            // }
+        }
+    
+
+        // cout<<ans<<"-->";
+        string result = ans;
+        reverse(ans.begin(), ans.end());
+        if(ans == result)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+
+
